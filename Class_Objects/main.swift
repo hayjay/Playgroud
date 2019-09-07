3817ab7380f98a7c98aca0db0447fb91e2a892b0
Nurudeen Ajayi <nurudeen.ajayi@techadvance.ng>
2019-09-07 17:44:39 +0100

    Initial Commit

---
+++ Class_Objects/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Class_Objects
+//
+//  Created by new on 07/09/2019.
+//  Copyright © 2019 LearnAppMaking. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
